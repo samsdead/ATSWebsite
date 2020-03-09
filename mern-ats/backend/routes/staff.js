b840cc07c68d0c4abcbb8ee10bd99164fdba8230
Said M <said.moredi@gmail.com>
2020-03-09 19:16:47 +0000

    staff up to date with original schema

--- mern-ats/backend/routes/staff.js
+++ mern-ats/backend/routes/staff.js
@@ -10,10 +10,28 @@ router.route('/').get((req, res) => {
 router.route('/add').post((req, res) => {
   const id = req.body.id;
   const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const dOB = req.body.dOB;
+  const telephoneNo = req.body.telephoneNo;
+  const email = req.body.email;
+  const commissionRate = req.body.commissionRate;
+  const userName = req.body.userName;
+  const passwordHash = req.body.passwordHash;
+  const addressID = req.body.addressID;
+  const commissionRateID = req.body.commissionRateID;
 
   const newStaff = new Staff({
     id,
     firstName,
+    lastName,
+    dOB,
+    telephoneNo,
+    email,
+    commissionRate,
+    userName,
+    passwordHash,
+    addressID,
+    commissionRateID
   });
 
   newStaff.save()
@@ -38,6 +56,14 @@ router.route('/update/:id').post((req, res) => {
     .then(staff => {
       staff.id = req.body.id;
       staff.firstName = req.body.firstName;
+      staff.lastName = req.body.lastName;
+      staff.dOB = req.body.dOB;
+      staff.telephoneNo = req.body.telephoneNo;
+      staff.email = req.body.email;
+      staff.commissionRate = req.body.userName;
+      staff.passwordHash = req.body.passwordHash;
+      staff.addressID = req.body.addressID;
+      staff.commissionRateID = req.body.commissionRateID;
 
       staff.save()
         .then(() => res.json('Staff updated!'))
