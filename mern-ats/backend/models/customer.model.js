3fccff4c1d47adad871b7b8d4270bfa33c8aeeaa
Said M <said.moredi@gmail.com>
2020-03-09 18:39:27 +0000

    customer and staff routes need update

--- mern-ats/backend/models/customer.model.js
+++ mern-ats/backend/models/customer.model.js
@@ -2,20 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const customerSchema = new Schema({
-  id: {
-    type: Number,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 10
-  },
-  firstName: {
-    type: String,
-    required: true,
-    unique: false,
-    minlength: 2
-  },
-},{
+  id: {type: Number, required: true, unique: true, trim: true, minlength: 10},
+  firstName: {type: String, required: true, unique: false, minlength: 2},
+  lastName: {type: String, required: true, unique: false, minlength: 2},
+  dOB: {type: Date},
+  telephoneNo: {type: Number, required: true, unique: true, trim: true, minlength: 10},
+  email: {type: String, unique: true},
+  isValued:{type: Boolean},
+  discountID: {type: Number, unique: true},
+  addressID:{type: Number, unique: true},
+  DiscountRateID: {type: Number, unique: true}
+},
+
+{
   timestamps: true,
 });
 
--- mern-ats/backend/models/staff.model.js
+++ mern-ats/backend/models/staff.model.js
@@ -2,20 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const staffSchema = new Schema({
-  id: {
-    type: Number,
-    required: true,
-    unique: true,
-    trim: true,
-    minlength: 10
-  },
-  firstName: {
-    type: String,
-    required: true,
-    unique: false,
-    minlength: 2
-  },
-},{
+  id: {type: Number, required: true, unique: true, trim: true, minlength: 10},
+  firstName: {type: String, required: true, minlength: 2},
+  lastName: {type: String, required: true, minlength: 2},
+  dOB:{type: Date},
+  telephoneNo: {type: Number, required: true, unique: true, trim: true, minlength: 10},
+  email: {type: String, unique: true},
+  commissionRate:{type: Number},
+  userName: {type: String, unique: true},
+  passwordHash: {type: String},
+  addressID: {type: Number},
+  commissionRateID: {type: Number},
+},
+  {
   timestamps: true,
 });
 
